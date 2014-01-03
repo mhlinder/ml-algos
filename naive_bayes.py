d3d3ee9110f584d354f45c977ee7b8c5d07d2145
mhlinder <mhlinder@gmail.com>
2014-01-03 18:42:32 -0500

    now classifies test training set

--- naive_bayes.py
+++ naive_bayes.py
@@ -11,15 +11,18 @@ Nk = 10 # number of digits depicted by an image
 l = 1 # laplace smoothing for l=1
 
 # read training data; split into train, test subsets
-data = read_csv('kaggle/train.csv')
-train = data[len(data)/10:]
-test = data[:len(data)/10]
+train = read_csv('kaggle/train.csv')
+test = read_csv('kaggle/test.csv')
+# data = read_csv('kaggle/train.csv')
+# train = data[len(data)/10:]
+# test = data[:len(data)/10]
 
 # initialize dicts for counting occurences
 counts_y = {i: 0 for i in range(Nk)} # counts for each y_k value
 counts = {i: zeros([Ni, Nj]) for i in range(Nk)} # counts for each pixel in each X_i
 
 # loop over training values, record each pixel's value
+n = len(train)
 for i in range(len(train)):
     d = train.iloc[i].values
     # record label (digit depicted)
@@ -33,18 +36,23 @@ for i in range(len(train)):
 
     # user interface
     if i % 1000 == 0:
-        sys.stdout.write("\rprocessed %i observations" % i)
+        sys.stdout.write("\rprocessed %i observations of %i" % (i, n))
         sys.stdout.flush()
+
 
 # convert counts to proportions
 pis = {i: counts_y[i] / float(len(train)) for i in range(Nk)} # proportion for each y_k value
 thetas = {i: (counts[i] + l) / float(counts_y[i] + l*Nj) for i in range(Nk)} # proportion for each X_i pixel
 
+print
+print "finished training data"
+
 # classify each item in test set
 test['guess'] = tile(nan, len(test))
+n = len(test)
 for t in range(len(test)):
     # remove label, guess columns
-    tt = test.iloc[t].values[1:-1]
+    tt = test.iloc[t].values[:-1]
     # initial class "guess"
     max_lh = [-inf, -inf]
 
@@ -64,3 +72,10 @@ for t in range(len(test)):
 
     # record class
     test['guess'].iloc[t] = max_lh[1]
+    
+    # user interface
+    if t % 1000 == 0:
+        sys.stdout.write("\rprocessed %i observations of %i" % (t, n))
+        sys.stdout.flush()
+
+test['guess'].to_csv('test_labels.csv',float_format="%.0f",header=True,index_label="ImageId")
