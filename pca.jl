bc339a40c6d56b13adeb73df38c717b30da00f92
mhlinder <mhlinder@gmail.com>
2014-01-04 16:16:15 -0500

    Sat Jan  4 16:16:15 EST 2014

--- pca.jl
+++ pca.jl
@@ -1,25 +1,26 @@
 # read in data
 data = readdlm("kaggle/train.csv", ',', Int64, has_header=true);
-train = data[1];
-header = data[2];
+train = data[1]
+header = data[2]
 
-pixels = train[:,2:];
+pixels = train[:,2:]
 
 # demean
 pixels_adj = zeros(Float64, size(pixels));
 for i = 1:size(pixels)[2]
-    m = mean(pixels[:,i]);
-    pixels_adj[:,i] = pixels[:,i] - m;
+    m = mean(pixels[:,i])
+    pixels_adj[:,i] = pixels[:,i] - m
 end
 
 # generate covariance matrix
-n = size(pixels_adj)[1];
-col_n = size(pixels_adj)[2];
-cov = zeros(Float64, col_n, col_n);
+n = size(pixels_adj)[1]
+col_n = size(pixels_adj)[2]
+cov = zeros(Float64, col_n, col_n)
 
 for i = 1:col_n
     for j = i:col_n
-        prod = pixels_adj[:,i] .* pixels[:,j];
+        # sample covariance of two different pixels
+        prod = pixels_adj[:,i] .* pixels[:,j]
         s_cov = sum(prod) / (n-1)
 
         cov[i,j] = s_cov
@@ -29,3 +30,20 @@ for i = 1:col_n
     end
     println(i)
 end
+
+# calculate eigenvectors and eigenvalues
+vals, vecs = eig(cov)
+sorted = sortperm(vals)
+
+vals = vals[sorted]
+vecs = vecs[:,sorted]
+
+# choose first n_pc principal components
+n_pc = 40
+vecs = vecs[:,1:n_pc]
+
+feature_vector = transpose(vecs)
+pixels_adj = transpose(pixels_adj)
+
+# dimension-reduced data
+final_data = feature_vector * pixels_adj
