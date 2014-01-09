0dca23177cb6433c944c5495dbedf69bbb7c9dc4
mhlinder <mhlinder@gmail.com>
2014-01-08 23:52:56 -0500

    started calculating naive bayes

--- pca_naive_bayes.R
+++ pca_naive_bayes.R
@@ -2,22 +2,26 @@ data1 <- read.csv('kaggle/train.csv',header=T)
 # data2 <- read.csv('kaggle/test.csv',header=T)
 data <- data.frame(data1)
 
+nK <- 10
+nPC <- 40
 
 # calculate principal components
 n <- nrow(data) / 10
-train <- data[n:nrow(data), 2:ncol(data)]
-pca <- princomp(train)
-pcs <- t(pca$loadings[,1:40])
+test = data[1:n,]
+train <- data[n:nrow(data),]
+pca <- princomp(train[,2:ncol(train)])
+pcs <- t(pca$loadings[,1:nPC]) # rows principal components
 
-# classify test set
-test <- data[1:n,]
-
+# conditional distributions look approximately normal
+# columns are digits; rows are PCs
+means <- mat.or.vec(nPC, nK)
+sds <- mat.or.vec(nPC, nK)
 for (digit in 0:9) {
-    d <- test[test[,1]==digit, 2:ncol(test)]
-    d <- t(d)
+    d <- train[train[,1]==digit,2:ncol(train)]
+    d <- t(d) # rows are explanatory variables
 
     pc <- pcs %*% d
-    pc <- t(pc)
+    pc <- t(pc) # rows are obs
 
     if (FALSE) { # Plot histograms for all digit, PC tuples
         for (com in 1:40)  {
@@ -25,4 +29,30 @@ for (digit in 0:9) {
             ggsave(file=sprintf('pngs/d%d_c%s.png', digit, com))
         }
     }
+
+    means[,digit+1] <- apply(pc, 2, mean)
+    sds[,digit+1] <- apply(pc, 2, sd)
 }
+
+# calculate log likelihood fo# r each test value, conditional on digit
+# llh <- mat.or.vec(nrow(test), nK) # log likelihoods
+# for (digit in 0:9) {
+    # # transform test values to principal components
+    # d <- t(test[,2:ncol(test)])
+
+    # pc <- pcs %*% d
+    # pc <- t(pc)
+
+    # # likelihood values
+    # probs <- matrix(0, nrow(pc), ncol(pc))
+
+    # # for each column (each PC), calculate the likelihood for that PC, conditional on digit
+    # for (i in 1:ncol(pc)) {
+        # probs[,i] <- dlnorm(pc[,i], mean=means[i,digit+1], sd=sds[i,digit+1])
+    # }
+    # # as dlnorm --- log normal --- was used, we sum over likelihood values
+    # llh[,digit] <- apply(probs, 1, sum)
+# }
+
+# # digit is index-1 of maximum likelihood
+# digits <- apply(llh, 1, which.max) - 1
